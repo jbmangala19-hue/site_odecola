809d9b598870574c3b7c9d0112c34e4873a3618a
monifort1 <oboramouessegladdemonifort@gmail.com>
2026-09-15 13:27:38 +0100

    correction 1 A

--- site_odecola/settings.py
+++ site_odecola/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'site_odecola.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
